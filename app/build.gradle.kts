ade4c074a0e0aa087e9233085a6e934966c470da
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2025-09-29 21:51:04 +0300

    :green_heart: (libs) explicitly add material icons extended

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,11 +97,11 @@ kotlin {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
